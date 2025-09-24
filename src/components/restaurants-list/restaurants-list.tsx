41765145ad407cc00cefc340ea4946f42c30aa3f
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-09-24 10:18:03 +0200

    fix(restaurants-list): changed grid layout to be more flexible across different screen sizes

--- src/components/restaurants-list/restaurants-list.tsx
+++ src/components/restaurants-list/restaurants-list.tsx
@@ -5,7 +5,10 @@ export default function RestaurantsList() {
   return (
     <section className="p-[1rem] w-full">
       <h1 className="mb-[1rem]">Restaurants</h1>
-      <div className="grid grid-cols-1 gap-[1rem] sm:grid-cols-[repeat(auto-fit,minmax(15rem,2fr))]">
+      <div className="grid grid-cols-1 gap-[1rem] sm:grid-cols-2 lg:grid-cols-3">
+        <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
+        <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
+        <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
         <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
         <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
         <RestaurantCard restaurantName="Cortado Bar" imageUrl="/images/coffee.png" />
