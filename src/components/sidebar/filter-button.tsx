a4761ce7369709759a8e62e7f79b15b7cd660090
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-09-26 16:25:48 +0200

    style(filter-button): add hover and active states

--- src/components/sidebar/filter-button.tsx
+++ src/components/sidebar/filter-button.tsx
@@ -9,7 +9,7 @@ interface FilterButtonProps {
 export default function FilterButton({ label, onClick, isSelected }: FilterButtonProps) {
   return (
     <button onClick={onClick}
-    className={`flex w-fit px-[0.75rem] py-[0.5rem] align-top border border-[var(--stroke)] rounded-lg bg-[var(--white)] cursor-pointer ${isSelected ? "border-selected" : "border-[var(--stroke)] bg-[var(--white)]"}`}>
+    className={`flex w-fit px-[0.75rem] py-[0.5rem] align-top border border-[var(--stroke)] rounded-lg bg-[var(--white)] cursor-pointer transition duration-300 hover:scale-[1.02] ${isSelected ? "border-selected" : "border-[var(--stroke)] bg-[var(--white)]"}`}>
       <h4>{label}</h4>
     </button>
   );
