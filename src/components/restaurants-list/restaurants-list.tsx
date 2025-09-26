01a71b88b7fee7e3b6254b92e28af2dd1ea384dc
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-09-26 16:05:13 +0200

    style(layout): adjust paddings for restaurant list and header

--- src/components/restaurants-list/restaurants-list.tsx
+++ src/components/restaurants-list/restaurants-list.tsx
@@ -54,8 +54,8 @@ useEffect(() => {
 
   return (
     <section className="p-[1rem] w-full">
-      <h1 className="mb-[1rem]">Restaurants</h1>
-      <div className="grid grid-cols-1 gap-[1rem] sm:grid-cols-2 lg:grid-cols-3">
+      <h1 className="mb-[1rem] py-[1rem]">Restaurants</h1>
+      <div className="grid grid-cols-1 gap-[1rem] py-[1rem] sm:grid-cols-2 lg:grid-cols-3">
         {restaurants.map((restaurant) => (
           <RestaurantCard
             key={restaurant.id}
