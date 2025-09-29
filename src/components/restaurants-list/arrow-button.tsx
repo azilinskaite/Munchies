14e4d8fcde784319b9afad013cfbce0fa5fc46df
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-09-29 10:41:38 +0200

    style(arrow-button): add a hover state

--- src/components/restaurants-list/arrow-button.tsx
+++ src/components/restaurants-list/arrow-button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ArrowButton() {
   return (
-    <button className="h-[2rem] w-[2rem] border-none rounded-full bg-[var(--green)] flex items-center justify-center shrink-0 cursor-pointer">
+    <button className="h-[2rem] w-[2rem] border-none rounded-full bg-[var(--green)] flex items-center justify-center shrink-0 transition duration-300 hover:scale-[1.02] cursor-pointer">
         <img src="/images/arrow.svg" alt="Arrow" width="12" height="auto" />
     </button>
   );
