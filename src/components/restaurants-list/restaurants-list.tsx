157e05fe264aa45111690504d5405567d6f3cab4
Adele Zilinskaite <zilinskaite.adele@gmail.com>
2025-09-29 11:00:45 +0200

    feat(restaurants-list): display error in restaurants list

--- src/components/restaurants-list/restaurants-list.tsx
+++ src/components/restaurants-list/restaurants-list.tsx
@@ -1,7 +1,10 @@
 "use client";
 import { useFilters } from "@/utils/hooks/FiltersContext";
 import React, { useState, useEffect } from "react";
-import { getRestaurantOpenStatus, getRestaurants } from "@/utils/api/restaurants";
+import {
+  getRestaurantOpenStatus,
+  getRestaurants,
+} from "@/utils/api/restaurants";
 import RestaurantCard from "./restaurant-card";
 import { applyFilters } from "@/utils/apply-filters";
 
@@ -24,48 +27,56 @@ export default function RestaurantsList({
   initialRestaurants: RestaurantWithStatus[];
 }) {
   const { selectedFilters } = useFilters();
-  const [restaurants, setRestaurants] = useState<RestaurantWithStatus[]>(initialRestaurants);
+  const [restaurants, setRestaurants] =
+    useState<RestaurantWithStatus[]>(initialRestaurants);
+  const [error, setError] = useState<string | null>(null);
 
-useEffect(() => {
-  async function fetchData() {
-    const allRestaurants = await getRestaurants();
-    // fetching all restaurants
+  useEffect(() => {
+    async function fetchData() {
+      setError(null);
 
-    // fetching open status for each restaurant
-    interface StatusResult extends Restaurant {
-      is_open: boolean;
+      try {
+        const allRestaurants = await getRestaurants();
+
+        const statusResults: RestaurantWithStatus[] = await Promise.all(
+          allRestaurants.map((r: Restaurant) =>
+            getRestaurantOpenStatus(r.id).then((is_open: boolean) => ({
+              ...r,
+              is_open,
+            }))
+          )
+        );
+
+        const filtered = applyFilters(statusResults, selectedFilters);
+        setRestaurants(filtered);
+      } catch (err: any) {
+        console.error("Error fetching restaurants:", err);
+        setError(
+          "😟 Sorry, we couldn't load restaurants at the moment. Please try again later."
+        );
+      } 
     }
 
-    const statusResults: StatusResult[] = await Promise.all(
-      allRestaurants.map((r: Restaurant) =>
-      getRestaurantOpenStatus(r.id).then((is_open: boolean): StatusResult => ({
-        ...r,
-        is_open,
-      }))
-      )
-    );
-
-    const filtered = applyFilters(statusResults, selectedFilters);
-    setRestaurants(filtered);
-  }
-
-  fetchData();
-}, [selectedFilters]);
+    fetchData();
+  }, [selectedFilters]);
 
   return (
     <section className="p-[1rem] lg:pr-[7.5rem] w-full">
       <h1 className="mb-[1rem] py-[1rem]">Restaurants</h1>
-      <div className="grid grid-cols-1 gap-[1rem] py-[1rem] sm:grid-cols-2 lg:grid-cols-3">
-        {restaurants.map((restaurant) => (
-          <RestaurantCard
-            key={restaurant.id}
-            restaurantName={restaurant.name}
-            imageUrl={restaurant.image_url}
-            isOpen={restaurant.is_open}
-            deliveryTimeMinutes={restaurant.delivery_time_minutes}
-          />
-        ))}
-      </div>  
+
+      {!error && (
+        <div className="grid grid-cols-1 gap-[1rem] py-[1rem] sm:grid-cols-2 lg:grid-cols-3">
+          {restaurants.map((restaurant) => (
+            <RestaurantCard
+              key={restaurant.id}
+              restaurantName={restaurant.name}
+              imageUrl={restaurant.image_url}
+              isOpen={restaurant.is_open}
+              deliveryTimeMinutes={restaurant.delivery_time_minutes}
+            />
+          ))}
+        </div>
+      )}
     </section>
   );
 }
